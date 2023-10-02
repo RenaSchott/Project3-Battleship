1b02ddb6c80e9b98730f7e620ae63a851be0088e
R S <rena_schott@gmx.de>
2023-10-02 14:16:23 +0000

    Initial commit for code in run.py and some credits saved in readme file

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pyfiglet
+
+WELCOME = pyfiglet.figlet_format("Welcome to Battleship!")
+#RULES = You can find the rules for Battleship 
+
+START = input("Do you want to start the game (y/n)?")
+
+#def start_game():
+    #try:
+        #if input == y:
+
+       # else:
+    #except:
+        
+
+def main():
+    print(WELCOME/n)
+    print(START)
+    #print(RULES)
+
+main()
