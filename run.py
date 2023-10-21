9f2c06e6d4c1361410152ffa26f58ff275383d1a
R S <rena_schott@gmx.de>
2023-10-21 13:25:19 +0000

    Worked on displaying a bordered board

--- run.py
+++ run.py
@@ -24,13 +24,21 @@ def board_creation():
 
 def print_board(board):
     """Give game boards the proper styling"""
-    print(f"{board[0][0]}{board[0][1]}{board[0][2]}{board[0][3]}{board[0][4]}{board[0][5]}{board[0][6]}")
-    print(f"{board[1][0]}{board[1][1]}{board[1][2]}{board[1][3]}{board[1][4]}{board[1][5]}{board[1][6]}")
-    print(f"{board[2][0]}{board[2][1]}{board[2][2]}{board[2][3]}{board[2][4]}{board[2][5]}{board[2][6]}")
-    print(f"{board[3][0]}{board[3][1]}{board[3][2]}{board[3][3]}{board[3][4]}{board[3][5]}{board[3][6]}")
-    print(f"{board[4][0]}{board[4][1]}{board[4][2]}{board[4][3]}{board[4][4]}{board[4][5]}{board[4][6]}")
-    print(f"{board[5][0]}{board[5][1]}{board[5][2]}{board[5][3]}{board[5][4]}{board[5][5]}{board[5][6]}")
-    print(f"{board[6][0]}{board[6][1]}{board[6][2]}{board[6][3]}{board[6][4]}{board[6][5]}{board[6][6]}")
+    print(f"|---------------------|")
+    print(f"|{board[0][0]}|{board[0][1]}|{board[0][2]}|{board[0][3]}|{board[0][4]}|{board[0][5]}|{board[0][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[1][0]}|{board[1][1]}|{board[1][2]}|{board[1][3]}|{board[1][4]}|{board[1][5]}|{board[1][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[2][0]}|{board[2][1]}|{board[2][2]}|{board[2][3]}|{board[2][4]}|{board[2][5]}|{board[2][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[3][0]}|{board[3][1]}|{board[3][2]}|{board[3][3]}|{board[3][4]}|{board[3][5]}|{board[3][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[4][0]}|{board[4][1]}|{board[4][2]}|{board[4][3]}|{board[4][4]}|{board[4][5]}|{board[4][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[5][0]}|{board[5][1]}|{board[5][2]}|{board[5][3]}|{board[5][4]}|{board[5][5]}|{board[5][6]}|")
+    print(f"|---------------------|")
+    print(f"|{board[6][0]}|{board[6][1]}|{board[6][2]}|{board[6][3]}|{board[6][4]}|{board[6][5]}|{board[6][6]}|")
+    print(f"|---------------------|")
 
 
 
@@ -46,10 +54,10 @@ def main():
     print(WELCOME)
     #print(START)
     #print(RULES)
-    board1 = board_creation()
-    board2 = board_creation()
-    print_board(board1)
-    print_board(board2)
+    board = board_creation()
+    #board2 = board_creation()
+    #print_board(board1)
+    print_board(board)
     #print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
 
