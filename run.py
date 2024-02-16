58c1ef3853db3b16b1f78d6d878ab624666a1975
R S <rena_schott@gmx.de>
2024-02-16 10:41:17 +0000

    Changed blank exeption to whitespace in the username input

--- run.py
+++ run.py
@@ -56,10 +56,10 @@ def get_username():
     Returns:
         username = Answer to the question
     """
-    username = input(f"A name is needed for playing the game.
+    username = input(f"A name is needed for playing the game.\
                      What is your name?\n")
-    if username == "":
-        print(f"A username is needed for playing the game.
+    if username.isspace():
+        print(f"The input contains only whitespace characters.\
               Please restart the game.")
         sys.exit(0)
     return username
@@ -142,25 +142,25 @@ def print_board(board):
     Returns:
         only print statements
     """
-    print(f" {board[0][0]} | {board[0][1]} | {board[0][2]} | {board[0][3]} |
+    print(f" {board[0][0]} | {board[0][1]} | {board[0][2]} | {board[0][3]} |\
           {board[0][4]} | {board[0][5]} | {board[0][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[1][0]} | {board[1][1]} | {board[1][2]} | {board[1][3]} |
+    print(f" {board[1][0]} | {board[1][1]} | {board[1][2]} | {board[1][3]} |\
           {board[1][4]} | {board[1][5]} | {board[1][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[2][0]} | {board[2][1]} | {board[2][2]} | {board[2][3]} |
+    print(f" {board[2][0]} | {board[2][1]} | {board[2][2]} | {board[2][3]} |\
           {board[2][4]} | {board[2][5]} | {board[2][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[3][0]} | {board[3][1]} | {board[3][2]} | {board[3][3]} |
+    print(f" {board[3][0]} | {board[3][1]} | {board[3][2]} | {board[3][3]} |\
           {board[3][4]} | {board[3][5]} | {board[3][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[4][0]} | {board[4][1]} | {board[4][2]} | {board[4][3]} |
+    print(f" {board[4][0]} | {board[4][1]} | {board[4][2]} | {board[4][3]} |\
           {board[4][4]} | {board[4][5]} | {board[4][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[5][0]} | {board[5][1]} | {board[5][2]} | {board[5][3]} |
+    print(f" {board[5][0]} | {board[5][1]} | {board[5][2]} | {board[5][3]} |\
           {board[5][4]} | {board[5][5]} | {board[5][6]} ")
     print(f"---|---|---|---|---|---|---")
-    print(f" {board[6][0]} | {board[6][1]} | {board[6][2]} | {board[6][3]} |
+    print(f" {board[6][0]} | {board[6][1]} | {board[6][2]} | {board[6][3]} |\
           {board[6][4]} | {board[6][5]} | {board[6][6]} ")
 
 
